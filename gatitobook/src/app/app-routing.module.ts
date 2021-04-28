aabb948c4ea7e04154569c69e036d913d161c4b3
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-04-28 16:09:57 -0300

    Criei um lazy-loading para HomeComponent;

--- gatitobook/src/app/app-routing.module.ts
+++ gatitobook/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { HomeModule } from './home/home.module';
+
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
