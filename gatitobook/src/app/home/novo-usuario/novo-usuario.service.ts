d3f67d0ed967cb383b3fd09b883c2154a8e11687
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-05-04 15:17:41 -0300

    Adicionei validação por reactive forms em registo.

---
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NovoUsuario } from './novo-usuario';
+
+const URL_API = 'http://localhost:3000'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(
+    private http: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario): Observable<any> {
+    return this.http.post(`${URL_API}/user/signup`, novoUsuario)
+  }
+
+}
