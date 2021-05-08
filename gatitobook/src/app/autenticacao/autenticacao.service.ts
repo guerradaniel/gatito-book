2344935fc2a60edbe31f21f2195dbdbf429cf8ca
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-05-07 23:01:53 -0300

    Criado service de token para autenticação.

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators';
 
 const URL_API = 'http://localhost:3000'
 
@@ -9,12 +11,20 @@ const URL_API = 'http://localhost:3000'
 })
 export class AutenticacaoService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private usuarioService: UsuarioService) { }
 
-  autenticar(usuario: string, senha: string): Observable<any> {
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
     return this.http.post(`${URL_API}/user/login`, {
       userName: usuario,
       password: senha
-    })
+    },
+      { observe: 'response' } // Traz o header da requisição
+    ).pipe(
+      tap((res) => {
+        const authToken = res.headers.get('x-access-token') ?? ''
+        this.usuarioService.salvaToken(authToken)
+      })
+    )
   }
 }
