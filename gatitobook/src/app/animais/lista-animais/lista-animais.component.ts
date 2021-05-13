a3045f2a995faaccb4b597e3773752cebe6231a8
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-05-13 20:15:51 -0300

    Adicionei um guard para a rota.

--- gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { Animais } from '../animais';
 import { AnimaisService } from '../animais.service';
@@ -20,6 +21,7 @@ export class ListaAnimaisComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    // Primeiro exemplo sem usar observable
     // this.usuarioService.retornaUsuario().subscribe((usuario) => {
     //   const userName = usuario.name ?? ''
     //   this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
@@ -39,3 +41,9 @@ export class ListaAnimaisComponent implements OnInit {
 
 
 // em "?? ''", atribui '' se for undefined ou nulo
+
+// o switchMap é utilizado para mudar fluxo. Até então o fluxo de dados vinha
+// de usuárioService. Para retornar os animais, precisamos de dados baseados
+// no usuarioService. Para isso é necessário retorná-lo no parâmetro definido
+// como usuario. 
+
