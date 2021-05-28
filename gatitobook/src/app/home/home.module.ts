826d6580d49f31970560b37d4b20770317cd82e9
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-05-27 22:58:56 -0300

    Criei o SharedModule para organizar o Module.

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -15,8 +16,7 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    MensagemModule
+    SharedModule
   ],
   exports: [
     HomeComponent
